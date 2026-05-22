e1cc0e5bb1eab8046d8679e01989e299d95cdb2c
Rustem <hyshikryst@gmail.com>
2026-05-22 17:46:42 +0500

    step 3 - костыль для деплоя

--- bin/run.sh
+++ bin/run.sh
@@ -3,12 +3,8 @@ set -euo pipefail
 
 echo "[run.sh] Starting service"
 
-if [ -z "${DATABASE_URL:-}" ]; then
-  echo "[run.sh] DATABASE_URL is empty, skipping migrations"
-else
-  echo "[run.sh] Running DB migrations"
-  goose -dir ./db/migrations postgres "${DATABASE_URL}" up
-fi
+echo "[run.sh] Running DB migrations"
+goose -dir ./db/migrations postgres "${DATABASE_URL}" up
 
 echo "[run.sh] Starting Go app"
 exec /app/bin/app
